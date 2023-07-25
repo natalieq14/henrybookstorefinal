e9eb901ba7337546ecc72e259f1b5a7251fcd8e1
Natalie Quesenberry <natalie.quesenberry@mga.edu>
2023-07-25 16:59:42 -0400

    finished!

--- app/henrybookstore/urls.py
+++ app/henrybookstore/urls.py
@@ -5,9 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    # path('books', views.BookView.as_view(), name='book'),
-    # path('branches', views.BranchView.as_view(), name='branch'),
-    # path('inventory/branch/<int:id>', views.BranchInventoryView.as_view(), name='branch_inventory'),
+    path('books', views.BookView.as_view(), name='book'),
+    path('branches', views.BranchView.as_view(), name='branch'),
+    path('inventory/branch/<int:id>', views.BranchInventoryView.as_view(), name='branch_inventory'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- app/henrybookstore/views.py
+++ app/henrybookstore/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 from django.template import loader
 from django.http import HttpResponse
 
+from .models import Book, Branch, Inventory
 
 # Create your views here.
 class HomePageView(View):
@@ -17,3 +18,45 @@ class HomePageView(View):
         return HttpResponse(template.render(context, request))
     
 
+class BookView(View):
+    def get(self, request):
+
+        template = loader.get_template('henrybookstore/books.html')     
+        books =   Book.objects.all()
+
+        context = {
+            'title': 'Henry Bookstore - Books',
+            'books': books,
+        }
+
+        return HttpResponse(template.render(context, request))
+    
+class BranchView(View):
+    def get(self, request):
+
+        template = loader.get_template('henrybookstore/branches.html')     
+        branches =   Branch.objects.all()
+
+        context = {
+            'title': 'Henry Bookstore - Branches',
+            'branches': branches,
+        }
+
+        return HttpResponse(template.render(context, request))
+    
+class BranchInventoryView(View):
+    def get(self, request, id):
+
+        template = loader.get_template('henrybookstore/branch_inventory.html')     
+        #branch = Branch.objects.filter(id=id)
+        branch = get_object_or_404(Branch, id = id)
+        inventory = Inventory.objects.filter(branch__id=id)
+        #books = Book.objects.filter(branch__id=id)
+
+        context = {
+            'title': 'Henry Bookstore - Branch',
+            'branch': branch.name,
+            'inventory': inventory,
+        }
+
+        return HttpResponse(template.render(context, request))
